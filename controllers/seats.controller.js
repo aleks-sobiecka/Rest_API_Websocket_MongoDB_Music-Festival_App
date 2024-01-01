a2d3ebcd374883915d2bb6f368e6f962014ddcdd
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-01-01 15:03:15 +0100

    Seats.controller postOne - bug

--- controllers/seats.controller.js
+++ controllers/seats.controller.js
@@ -13,7 +13,7 @@ exports.getById = async (req, res) => {
     try {
         const seat = await Seat.findById(req.params.id);
         if(!seat) res.status(404).json({ message: 'Seat not found' });
-        else res.json(concert);
+        else res.json(seat);
       }
       catch(err) {
         res.status(500).json({ message: err });
@@ -25,10 +25,15 @@ exports.postOne = async (req, res) => {
         const { day, seat, client, email } = req.body;
         const newSeat = new Seat({ day: day, seat: seat, client: client, email: email });
         await newSeat.save();
+        //emitt new updated taken seats to all sockets
+        req.io.emit('seatsUpdated', await Seat.find());
         res.json({ message: 'OK' });
+        
+      
       } catch(err) {
         res.status(500).json({ message: err });
       }
+      
 };
 
 exports.putById = async (req, res) => {
