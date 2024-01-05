86bfba4dafb469043d7a624fbf64925bec572418
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-01-05 12:48:13 +0100

    Add new endpoints for concerts search

--- controllers/concerts.controller.js
+++ controllers/concerts.controller.js
@@ -20,6 +20,52 @@ exports.getById = async (req, res) => {
       }
 };
 
+exports.getByPerformer = async (req, res) => {
+    try {
+      const concert = await Concert.find({performer: req.params.performer});
+      if(!concert) res.status(404).json({ message: 'Concert not found' });
+      else res.json(concert);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+};
+
+exports.getByGenre = async (req, res) => {
+    try {
+      const concert = await Concert.find({genre: req.params.genre});
+      if(!concert) res.status(404).json({ message: 'Concert not found' });
+      else res.json(concert);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+};
+
+exports.getByDay = async (req, res) => {
+    try {
+      const concert = await Concert.find({day: req.params.day});
+      if(!concert) res.status(404).json({ message: 'Concert not found' });
+      else res.json(concert);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+};
+
+exports.getByPrice = async (req, res) => {
+    try {
+      const concert = await Concert.find({"price": { $gte: req.params['price_min'], $lte: req.params['price_max'] },
+     });
+      if(!concert) res.status(404).json({ message: 'Concert not found' });
+      else res.json(concert);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }
+};
+
+
 exports.postOne = async (req, res) => {
     try {
         const { performer, genre, price, day, image } = req.body;
--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -4,6 +4,11 @@ const ConcertController = require('../controllers/concerts.controller');
 
 router.get('/concerts', ConcertController.getAll);
 router.get('/concerts/:id', ConcertController.getById);
+router.get('/concerts/performer/:performer', ConcertController.getByPerformer);
+router.get('/concerts/genre/:genre', ConcertController.getByGenre);
+router.get('/concerts/price/day/:day', ConcertController.getByDay);
+router.get('/concerts/price/:price_min/:price_max', ConcertController.getByPrice);
+
 router.post('/concerts', ConcertController.postOne);
 router.put('/concerts/:id', ConcertController.putById);
 router.delete('/concerts/:id', ConcertController.deleteById);  
